5537852f8a92ccb740a262ed95cbdeba27b41eee
levideutsch <levideutsch@live.com>
2022-09-30 17:10:54 -0400

    commit tree 139248b8b018fed68007c21b762290f0e0750f47

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+const removeMain = document.getElementById("main") // This will remove the "main" element
+removeMain.remove()
+// console.log(removeMain)
+
+const newHeader = document.createElement("h1") // This will create the "h1" element
+// document.body.append(newHeader)
+
+newHeader.id = 'victory' // This will add the id of "victory" to our "h1" element
+
+newHeader.textContent = "YOUR-NAME is the champion"
+// newHeader.append(newText)
